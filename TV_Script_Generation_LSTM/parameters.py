3cf722bdabd77d90e83a5a84643a43f0801cc705
Ankit Jain <asj.ankit@gmail.com>
2018-09-02 11:16:54 -0700

    Correcting the parameters file

--- TV_Script_Generation_LSTM/parameters.py
+++ TV_Script_Generation_LSTM/parameters.py
@@ -4,9 +4,9 @@ This file contains all the parameters required for running the model
 
 
 # Training Parameters
-NUM_EPOCHS = 1
+NUM_EPOCHS = 500
 LEARNING_RATE = 0.001 # Learning Rate
-BATCH_SIZE = 512 # Batch Size
+BATCH_SIZE = 128 # Batch Size
 CHECKPOINT_PATH_DIR = './model_dir'
 RESTORE_TRAINING=False
 SAVE_DIR = './save'
