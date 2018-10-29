b4e988f407a18ca803fbdceb4c5459d6034b63eb
Ankit Jain <asj.ankit@gmail.com>
2018-10-28 23:01:53 -0700

    Bayesian Neural Networks

---
+++ Bayesian_Neural_Networks/parameters.py
@@ -0,0 +1,13 @@
+
+
+DATA_DIR = '/Users/ankit.jain/Documents/Teaching&Learning/Packt/Book/BNN/Data'
+NUM_CLASSES = 43
+IMG_SIZE = 32
+
+#Training Parameters
+BATCH_SIZE =128
+EPOCHS =1000
+LEARNING_RATE = 0.001
+
+# Inference Parameters
+NUM_MONTE_CARLO = 50
